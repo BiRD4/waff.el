df9799b485ff2fb8e08896ac8132c11fe7a43a2a
Boone Douglass <boone@boonedouglass.com>
2021-09-25 18:47:26 -0400

    Add keymap

--- waff.el
+++ waff.el
@@ -1,1 +1,10 @@
 ;;; waff.el --- Tasty Emacs waffles
+
+(defvar waffle-mode-map
+  (let ((map (make-sparse-keymap)))
+	(define-key map "f" 'waffle-fill)
+	(define-key map "l" 'waffle-flip)
+	(define-key map "r" 'waffle-remove)
+	(define-key map "e" 'waffle-eat)
+	map)
+  "Keymap for `waffle-mode'.")
