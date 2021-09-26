af31b5eee2c017d9fb6fd3e52fc3bb8e2a2028f3
Boone Douglass <boone@boonedouglass.com>
2021-09-26 07:28:21 -0400

    Change cook time

--- waff.el
+++ waff.el
@@ -171,9 +171,9 @@
   "Eat waffle!"
   (interactive)
   (if waffle-plate-filled
-	  (progn (if (< 170 (max waffle-plate-cooked1 waffle-plate-cooked2))
+	  (progn (if (< 40 (max waffle-plate-cooked1 waffle-plate-cooked2))
 				 (message "*Chrunch* ...Wow, that one sure was toasty!")
-			   (if (> 130 (min waffle-plate-cooked1 waffle-plate-cooked2))
+			   (if (> 20 (min waffle-plate-cooked1 waffle-plate-cooked2))
 				   (message "*Mush* ...mmmm... tastes like... salmonella...")
 				 (message "Waffle eaten!")))
 			 (setq-local waffle-plate-filled nil)
